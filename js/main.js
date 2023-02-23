458bf36c7b93836541e943dc5c53089bb1a79ae0
mikaeltreppo <mikaeltreppo@outlook.com>
2023-02-23 16:14:09 +0100

    inserimento costo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 
+document.getElementById('km').innerHTML = km;
+
+
+
+const priceKm = 0.21;
+let totPrice = priceKm * km;
+let scontoSenior = (40 / 100) * totPrice;
+let spesaSenior = totPrice - scontoSenior;
+let scontoJunior = (20 / 100) * totPrice;
+let spesaJunior = totPrice - scontoJunior;
+
 const confirmDom = document.getElementById('confirm');
+
+
 //mi dice i km inseriti
 confirmDom.addEventListener('click',
 
@@ -18,18 +31,58 @@ confirmDom.addEventListener('click',
         document.getElementById('tableName').innerHTML = tellName;
     }
 )
+// mi dice la tipologia di sconto
 confirmDom.addEventListener('click',
 
     function () {
         const valuesDom = document.getElementById('select');
         const agePassenger = valuesDom.value;
- 
-      if(agePassenger == 'maggiorenne')
-      document.getElementById('offert').innerHTML = 'Nessuno sconto';
-      
-      else if (agePassenger == "minorenne")
-      document.getElementById('offert').innerHTML = "Sconto Junior 20%";
-      else(agePassenger == 'over65')
-      document.getElementById('offert').innerHTML = "Sconto Premorte 40%";
+
+        if (agePassenger == 'maggiorenne')
+            document.getElementById('offert').innerHTML = 'Nessuno sconto';
+
+        else if (agePassenger == "minorenne")
+            document.getElementById('offert').innerHTML = "Sconto Junior 20%";
+        else if (agePassenger == 'over65')
+            document.getElementById('offert').innerHTML = "Sconto Over 40%";
     }
 )
+//codice biglietto generator
+confirmDom.addEventListener('click',
+    function () {
+
+        const ticketNumber = Math.floor(Math.random() * 90001);
+        document.getElementById('ticketCode').innerHTML = ticketNumber;
+    })
+
+//numero seduta generator
+confirmDom.addEventListener('click',
+    function () {
+
+        const seatNumber = Math.floor(Math.random() * 56);
+        document.getElementById('seatN').innerHTML = seatNumber;
+    })
+
+
+
+
+
+
+
+
+
+//costo biglietto
+confirmDom.addEventListener('click',
+    function () {
+        const valuesDom = document.getElementById('select');
+        const agePassenger = valuesDom.value;
+
+        if (agePassenger == 'maggiorenne')
+            document.getElementById('cost').innerHTML = parseInt(totPrice);
+
+        else if (agePassenger == "minorenne")
+            document.getElementById('cost').innerHTML =parseInt(spesaJunior);
+        else if (agePassenger == 'over65')
+            document.getElementById('cost').innerHTML = spesaSenior;
+    }
+    )
