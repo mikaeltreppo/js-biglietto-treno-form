32bf415a497c33b0c1bdea89fa9c40d1109f9612
mikaeltreppo <mikaeltreppo@outlook.com>
2023-02-23 15:36:56 +0100

    inserito tipologia sconto

--- js/main.js
+++ js/main.js
@@ -9,12 +9,27 @@ confirmDom.addEventListener('click',
         console.log(tellKm);
     }
 )
-// mi dice il nome e cognome del passeggero
+// mi dice il nome e cognome del passeggero e lo stampa al posto giusto
 confirmDom.addEventListener('click',
 
     function () {
         const namePassenger = document.getElementById('name');
         const tellName = namePassenger.value;
         document.getElementById('tableName').innerHTML = tellName;
+    }
+)
+confirmDom.addEventListener('click',
+
+    function () {
+        const valuesDom = document.getElementById('select');
+        const agePassenger = valuesDom.value;
+ 
+      if(agePassenger == 'maggiorenne')
+      document.getElementById('offert').innerHTML = 'Nessuno sconto';
+      
+      else if (agePassenger == "minorenne")
+      document.getElementById('offert').innerHTML = "Sconto Junior 20%";
+      else(agePassenger == 'over65')
+      document.getElementById('offert').innerHTML = "Sconto Premorte 40%";
     }
 )
